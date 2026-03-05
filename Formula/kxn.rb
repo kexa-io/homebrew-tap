95370cf43702462d93742e08914d52bacae5f88a
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-03-05 14:50:02 +0100

    kxn 0.1.0: use GitHubPrivateRepositoryReleaseDownloadStrategy

--- Formula/kxn.rb
+++ Formula/kxn.rb
@@ -6,18 +6,21 @@ class Kxn < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/kexa-io/kxn/releases/download/v0.1.0/kxn-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/kexa-io/kxn/releases/download/v0.1.0/kxn-x86_64-apple-darwin.tar.gz",
+          using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "d4c9177e8dd987e75e77ebd938358800a24df26d279d3f8fbb335740210950a7"
     end
     on_arm do
-      url "https://github.com/kexa-io/kxn/releases/download/v0.1.0/kxn-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/kexa-io/kxn/releases/download/v0.1.0/kxn-aarch64-apple-darwin.tar.gz",
+          using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "c4bd6ab62ee50c13e84a7c365fbb8e9c426342b7d5f4b198419e7f83018e1c88"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kexa-io/kxn/releases/download/v0.1.0/kxn-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/kexa-io/kxn/releases/download/v0.1.0/kxn-x86_64-unknown-linux-gnu.tar.gz",
+          using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "a3f43f0330a63eb4d88671cad7803f04f790231f08a15c336f95296c9eb45a65"
     end
   end
