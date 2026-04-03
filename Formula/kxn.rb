683ffb666b42640ef770de6db892b0528df872ad
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-04-03 09:03:36 +0000

    kxn 0.27.1

--- Formula/kxn.rb
+++ Formula/kxn.rb
@@ -1,28 +1,28 @@
 class Kxn < Formula
   desc "Kexa Next Gen — multi-cloud compliance scanner in Rust"
   homepage "https://github.com/kexa-io/kxn"
-  version "0.27.0"
+  version "0.27.1"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.0/kxn-x86_64-apple-darwin.tar.gz"
-      sha256 "b5b02164fecdd40414b03d3d2a5d57140909093e1ee6dd71fe88c3a77d8176b8"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.1/kxn-x86_64-apple-darwin.tar.gz"
+      sha256 "3f7718127967d767b9a34e603121d161fa5c19da5685fc2b437b1e90aa49c62e"
     end
     on_arm do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.0/kxn-aarch64-apple-darwin.tar.gz"
-      sha256 "dfda8f90e7f6af5ba9201baa7995d2db7b04b9d1334b7b268a3fa35894dbd26a"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.1/kxn-aarch64-apple-darwin.tar.gz"
+      sha256 "7bee77b78de39994e1b074def9a8bbfe83742bfc69fe94c907f733d944c9abe5"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.0/kxn-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "383e58a2a027ea17ba3bcbc75ba19fcba099740e0ff9adf94d23407959727fc4"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.1/kxn-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "981143734553c6d27868b9b5b0d85d5d6dd4ed9facbb85a5d63e2acdb439e9e1"
     end
     on_arm do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.0/kxn-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "f38fb1169d7f2b519dacef0b05211291acccb3a6bedacac1aba8196b5b4fb451"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.27.1/kxn-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7f5d617320dd0153d3f192b35e0e8a7169141e45329d12341533ad767cd7c448"
     end
   end
 
