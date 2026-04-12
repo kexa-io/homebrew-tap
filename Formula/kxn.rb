c3cf1a0d8a4584702c4dca2e7ce934651e0124e8
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-04-12 14:28:45 +0000

    kxn 0.30.0

--- Formula/kxn.rb
+++ Formula/kxn.rb
@@ -1,28 +1,28 @@
 class Kxn < Formula
   desc "Kexa Next Gen — multi-cloud compliance scanner in Rust"
   homepage "https://github.com/kexa-io/kxn"
-  version "0.29.0"
+  version "0.30.0"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.29.0/kxn-x86_64-apple-darwin.tar.gz"
-      sha256 "e7badd2b4efeb259a7adbaad58f5a6724bf62b31842cd8947fb3b8333af4cc40"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.30.0/kxn-x86_64-apple-darwin.tar.gz"
+      sha256 "93f906dba264708bc1ebf788e6532692979b7c910a0a71bc767160e0f7413ac3"
     end
     on_arm do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.29.0/kxn-aarch64-apple-darwin.tar.gz"
-      sha256 "b2f9a61ef06a028420c0680fcb3744def7c339bb02b94e9b103176e16beccb08"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.30.0/kxn-aarch64-apple-darwin.tar.gz"
+      sha256 "1e31cdde0db5c85bca7e0b7245bd245646a7d167c73a4e8fe679fce56549bec9"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.29.0/kxn-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "268bd0896b1bd92703fc3a510f56cbe4c3aaf4ce7371b43d9ead64eb59af75fb"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.30.0/kxn-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f2c8807512d2e16e167ce27716ce63140ecd33b02f3605636f51cfa3a809a59c"
     end
     on_arm do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.29.0/kxn-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "1b3bb0f6bfd8d8f465f07ac112de58ac648ce278c40daf470d533369d8a625de"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.30.0/kxn-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "c1650d2b7aa472cbf0a4a2dc0f98f9e2ed059aa3d8bc2950f787bca99da762ad"
     end
   end
 
