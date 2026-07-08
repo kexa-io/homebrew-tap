468e36852f2e8e779b1e4a71a9901790efcd39a8
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-07-08 19:21:12 +0000

    kxn 0.49.0

--- Formula/kxn.rb
+++ Formula/kxn.rb
@@ -7,22 +7,22 @@ class Kxn < Formula
   on_macos do
     on_intel do
       url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-x86_64-apple-darwin.tar.gz"
-      sha256 "0cd6b0e8cf516eace8387b13a5f1a547204fefe215f28a42b5032c0d1062af4f"
+      sha256 "3b2ee5ba3619a41653678d59946384d116d2d5dd84b5b6b7fe3d96de893a4e10"
     end
     on_arm do
       url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-aarch64-apple-darwin.tar.gz"
-      sha256 "2fd1543fc3b65cbf904357dc9b0e5833a040c606f5e5d469ccbe616accfebb77"
+      sha256 "bdf14e271015c2285974e6408959b0127ffb7dc63b5ec9cca95d2e1fd3b89f79"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "845ca46f1c449da44ac9ce5dfd0ab78e15e344ad19c0a37163ca86245190f2f0"
+      sha256 "010bf9db5577a957dd17092128c074e80233906f08c86f80b2e28ebf7c7e51fb"
     end
     on_arm do
       url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a8dbfa30f3e2a94139ea6adc1e3fb60d2f2bbf4823eadae4d4d1e575ce15a14f"
+      sha256 "35ef803d3fbdcfa913d5d82f0e231b4b3bbe32e47e28cf55a995289d096dc748"
     end
   end
 
