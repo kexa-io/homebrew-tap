49930c1ffff0b94d9b00a2f4abb2eb44537c88b1
patrick szymkowiak <52030887+pszymkowiak@users.noreply.github.com>
2026-07-08 19:04:19 +0000

    kxn 0.49.0

--- Formula/kxn.rb
+++ Formula/kxn.rb
@@ -1,28 +1,28 @@
 class Kxn < Formula
   desc "Kexa Next Gen — multi-cloud compliance scanner in Rust"
   homepage "https://github.com/kexa-io/kxn"
-  version "0.39.0"
+  version "0.49.0"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.39.0/kxn-x86_64-apple-darwin.tar.gz"
-      sha256 "8d6ac3167759ad92ddfb57ea2c76a2a0845448c1a466a33758b1270a0a64f601"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-x86_64-apple-darwin.tar.gz"
+      sha256 "0cd6b0e8cf516eace8387b13a5f1a547204fefe215f28a42b5032c0d1062af4f"
     end
     on_arm do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.39.0/kxn-aarch64-apple-darwin.tar.gz"
-      sha256 "32841fc86599532c96b58ffaa25501870d00ea2da7bb7acc07c535b7e32008c6"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-aarch64-apple-darwin.tar.gz"
+      sha256 "2fd1543fc3b65cbf904357dc9b0e5833a040c606f5e5d469ccbe616accfebb77"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.39.0/kxn-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "117d1d1d9e752ecebe21a392ec5923f9edd926feb1b86cd5043085a3b4209aa9"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "845ca46f1c449da44ac9ce5dfd0ab78e15e344ad19c0a37163ca86245190f2f0"
     end
     on_arm do
-      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.39.0/kxn-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8e6271f2cc8412420bfa086cce7dd5f88493b23bc33dae2397156538ce3fa84a"
+      url "https://github.com/kexa-io/homebrew-tap/releases/download/v0.49.0/kxn-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a8dbfa30f3e2a94139ea6adc1e3fb60d2f2bbf4823eadae4d4d1e575ce15a14f"
     end
   end
 
